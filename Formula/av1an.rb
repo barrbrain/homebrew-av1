28d386a642f39091f5dd52d2389853a8bcf73dc1
David Michael Barr <b@rr-dav.id.au>
2021-11-03 18:22:15 +0000

    av1an: add 0.2.0 bottle.

--- Formula/av1an.rb
+++ Formula/av1an.rb
@@ -11,6 +11,12 @@ class Av1an < Formula
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/barrbrain/homebrew-av1/releases/download/av1an-0.2.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "8d320638077271772c2fbae35a712a92a84b44729bc47a9d1b9450b89d406df4"
+  end
+
   depends_on "nasm" => :build
   depends_on "rust" => :build
   depends_on "ffmpeg-libvmaf"
